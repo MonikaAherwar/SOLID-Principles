4d2b348ba2cd3b9731d41160968080b7e09ad046
MonikaAherwar <monikaaherwar@gmail.com>
2023-09-07 12:15:37 +0530

    InterfaceSegregationPriciple

---
+++ SOLID-Principles/InterfaceSegregationPrinciple/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principles.InterfaceSegregationPrinciple
+{
+    internal class InterfaceSegregationPrinciple
+    {
+        public InterfaceSegregationPrinciple()
+        {
+            Console.WriteLine("**************** DEFINITION ********************");
+            Console.WriteLine("The Interface Segregation Principle (ISP) states that " +
+                "clients should not be forced to depend on interfaces they do not use. " +
+                "In other words, it's better to have smaller, more specific interfaces " +
+                "rather than a single large interface that contains methods unrelated to a client's needs.");
+            Console.WriteLine("");
+        }
+
+        public void Problem()
+        {
+            Console.WriteLine("**************** PROBLEM ***********************");
+            Console.WriteLine("One interface contains more responsibility");
+            Console.WriteLine("");
+        }
+
+        public void Solution()
+        {
+            Console.WriteLine("**************** SOLUTION **********************");
+            Console.WriteLine("Divide the interface into smaller interfaces to divide the responsibility");
+            Console.WriteLine("");
+        }
+
+        public void Example()
+        {
+            Console.WriteLine("**************** EXAMPLE ***********************");
+            Console.WriteLine("Refer Strategy Pattern Example");
+            Console.WriteLine("");
+        }
+    }
+}
